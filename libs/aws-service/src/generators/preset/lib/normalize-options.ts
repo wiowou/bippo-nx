949ba82e6e5f3746bffd282619e8ff84b7db8659
wiowou <wiowou@users.noreply.github.com>
2023-06-08 23:07:04 -0400

    fix dependency

--- libs/aws-service/src/generators/preset/lib/normalize-options.ts
+++ libs/aws-service/src/generators/preset/lib/normalize-options.ts
@@ -1,7 +1,7 @@
 import { extractLayoutDirectory, Tree } from '@nx/devkit';
 import { getWorkspaceLayout, joinPathFragments, names } from '@nx/devkit';
 import { Linter } from '@nx/linter';
-import type { Schema as NodeApplicationGeneratorOptions } from '@nx/node/src/generators/application/schema';
+// import type { Schema as NodeApplicationGeneratorOptions } from '@nx/node/src/generators/application/schema';
 import type { PresetGeneratorSchema, NormalizedOptions } from '../schema';
 
 export function normalizeOptions(tree: Tree, options: PresetGeneratorSchema): NormalizedOptions {
@@ -25,21 +25,21 @@ export function normalizeOptions(tree: Tree, options: PresetGeneratorSchema): No
   };
 }
 
-export function toNodeApplicationGeneratorOptions(options: NormalizedOptions): NodeApplicationGeneratorOptions {
-  return {
-    name: options.name,
-    directory: options.directory,
-    frontendProject: options.frontendProject,
-    linter: options.linter,
-    skipFormat: true,
-    skipPackageJson: options.skipPackageJson,
-    // standaloneConfig: options.standaloneConfig,
-    tags: options.tags,
-    unitTestRunner: options.unitTestRunner,
-    e2eTestRunner: options.e2eTestRunner,
-    setParserOptionsProject: options.setParserOptionsProject,
-    rootProject: options.rootProject,
-    bundler: 'webpack', // Some features require webpack plugins such as TS transformers
-    isNest: true,
-  };
-}
+// export function toNodeApplicationGeneratorOptions(options: NormalizedOptions): NodeApplicationGeneratorOptions {
+//   return {
+//     name: options.name,
+//     directory: options.directory,
+//     frontendProject: options.frontendProject,
+//     linter: options.linter,
+//     skipFormat: true,
+//     skipPackageJson: options.skipPackageJson,
+//     // standaloneConfig: options.standaloneConfig,
+//     tags: options.tags,
+//     unitTestRunner: options.unitTestRunner,
+//     e2eTestRunner: options.e2eTestRunner,
+//     setParserOptionsProject: options.setParserOptionsProject,
+//     rootProject: options.rootProject,
+//     bundler: 'webpack', // Some features require webpack plugins such as TS transformers
+//     isNest: true,
+//   };
+// }
--- libs/aws-service/src/generators/preset/schema.d.ts
+++ libs/aws-service/src/generators/preset/schema.d.ts
@@ -3,14 +3,12 @@ import { UnitTestRunner } from '../utils';
 export interface PresetGeneratorSchema {
   name: string;
   directory?: string;
-  frontendProject?: string;
   linter?: Linter;
   skipFormat?: boolean;
   skipPackageJson?: boolean;
   tags?: string;
   unitTestRunner?: UnitTestRunner;
   e2eTestRunner?: 'jest' | 'none';
-  setParserOptionsProject?: boolean;
   rootProject?: boolean;
   strict?: boolean;
 }

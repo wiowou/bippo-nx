322f42295d90542f2e24d6d35d70629e2275686d
wiowou <wiowou@users.noreply.github.com>
2023-06-10 00:56:31 -0400

    fix template and type

--- libs/aws-service/src/generators/lambda-api/schema.d.ts
+++ libs/aws-service/src/generators/lambda-api/schema.d.ts
@@ -1,7 +1,8 @@
+import { Database } from '@bippo-nx/types';
 import { TerraformOptions } from '@bippo-nx/terraform';
 
 export interface LambdaApiGeneratorSchema extends TerraformOptions {
   name: string;
-  database: string;
+  database: Database;
   directory?: string;
 }

cfdb50af550ee539d0fe2cb24d62a2ca06a1ebec
wiowou <wiowou@users.noreply.github.com>
2024-05-03 22:16:35 -0400

    fix executor file replace

--- libs/dotnet/src/executors/lambda-package/lib/replace-files.ts
+++ libs/dotnet/src/executors/lambda-package/lib/replace-files.ts
@@ -2,10 +2,18 @@ import * as path from 'path';
 import { copyFile } from 'node:fs/promises';
 import { FileReplacement } from '../schema';
 
-export async function replaceFiles(fileReplacements: FileReplacement[], root: string) {
+import { NormalizedLambdaPackageExecutorSchema } from '../schema';
+
+export async function replaceFiles(
+  fileReplacements: FileReplacement[],
+  options: NormalizedLambdaPackageExecutorSchema
+) {
   for (const fileReplacement of fileReplacements) {
     try {
-      await copyFile(path.join(root, fileReplacement.with), path.join(root, fileReplacement.replace));
+      await copyFile(
+        path.join(options.sourceRoot, fileReplacement.with),
+        path.join(options.sourceRoot, fileReplacement.replace)
+      );
     } catch (e) {
       console.error(`Could not replace ${fileReplacement.replace} with ${fileReplacement.with}`, e);
     }
--- libs/terraform/src/executors/tfexec/lib/replace-files.ts
+++ libs/terraform/src/executors/tfexec/lib/replace-files.ts
@@ -1,11 +1,11 @@
 import * as path from 'path';
 import { copyFile } from 'node:fs/promises';
-import { FileReplacement } from '../schema';
+import { FileReplacement, NormalizedTfexecExecutorSchema } from '../schema';
 
-export async function replaceFiles(fileReplacements: FileReplacement[], root: string) {
+export async function replaceFiles(fileReplacements: FileReplacement[], options: NormalizedTfexecExecutorSchema) {
   for (const fileReplacement of fileReplacements) {
     try {
-      await copyFile(path.join(root, fileReplacement.with), path.join(root, fileReplacement.replace));
+      await copyFile(path.join(options.cwd, fileReplacement.with), path.join(options.cwd, fileReplacement.replace));
     } catch (e) {
       console.error(`Could not replace ${fileReplacement.replace} with ${fileReplacement.with}`, e);
     }
